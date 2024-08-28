3c8dfff8a1db564227d7072b673f58ab03abb446
mhrafi21 <12mhrafi@gmail.com>
2024-08-29 02:34:44 +0600

    some functionality added

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app: Application = express()
 
 // parser
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 // application routes
 
